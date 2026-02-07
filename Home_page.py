3c3d9975812921c8b4b8babab7192bd782533491
diracsportech0 <dirac.sportech@gmail.com>
2026-02-07 14:43:40 -0500

    commit firts

---
+++ Home_page.py
@@ -0,0 +1,19 @@
+import streamlit as st
+import numpy as np
+import pandas as pd
+
+#st.set_page_config(layout="wide")
+
+# ENCABEZADO: escudo Melgar + escudo Liga1
+colA, colB, colC = st.columns([1, 7, 1])
+with colA:
+    st.image('logo-tclub.png', use_column_width=True)
+with colB:
+    pass
+with colC:
+    st.image('logo-piad.png', use_column_width=True)
+    pass
+
+#FORMATO
+st.header(f'Buenas noches DT Darwin Chalco!!')
+
