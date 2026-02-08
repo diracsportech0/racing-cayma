4f2e169ab6c1f8d90e9b8665a8f244d9c5f3f2bd
diracsportech0 <dirac.sportech@gmail.com>
2026-02-07 19:50:44 -0500

    quitar output - de los graficos

--- functions.py
+++ functions.py
@@ -410,7 +410,7 @@ def passmap_player(df_pass,player,oponente):
 def barras_apiladas(df, x_col, fases_select,subtypes, titulo):    
     # 1. Filtramos los datos vacíos
     df_filtrado = df[df.output != '-']
-    df_filtrado = df[df.Fase.isin(fases_select)]
+    df_filtrado = df_filtrado[df_filtrado.Fase.isin(fases_select)]
 
     # 2. Agrupamos los datos para sumar las ocurrencias
     # .size() cuenta las filas, .reset_index() lo convierte de nuevo en DataFrame
