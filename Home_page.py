c16dca301a5f34895f3b13f07a5c1be4464c1b4d
diracsportech0 <dirac.sportech@gmail.com>
2026-02-07 19:10:21 -0500

    add events

--- Home_page.py
+++ Home_page.py
@@ -7,7 +7,7 @@ import pandas as pd
 # ENCABEZADO: escudo Melgar + escudo Liga1
 colA, colB, colC = st.columns([1, 7, 1])
 with colA:
-    st.image('logo-tclub.png', use_column_width=True)
+    st.image('logo-club.png', use_column_width=True)
 with colB:
     pass
 with colC:
